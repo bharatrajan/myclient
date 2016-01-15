2ddefe997d36c89d5e25de0d15f2b32ea9cf08d2
Bharat Rajan <brajan@ua.edu>
2016-01-15 09:50:57 -0800

    Delete action added

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,5 @@
 //Controller
-mainApp.controller('table_controller', function($scope, $http, getBookListService, addBookServ) {
+mainApp.controller('table_controller', function($scope, $http, getBookListService, addBookServ, delBookServ) {
 		
 	//Using getBookListService 
 	getBookListService.getBooks().then(function(responsBooks){
@@ -18,23 +18,26 @@ mainApp.controller('table_controller', function($scope, $http, getBookListServic
 			
 			//Calling service
 			addBookServ.add(book).then(function(data){
-					//SUCCESS : Updated Table displays Creating new book	
+					//SUCCESS : Updated Table displays Creating new book;	
 					if('success' == data.response.message && 'null' != data.item[0].id)
 						getBookListService.getBooks().then(function(responsBooks){
 							$scope.books = responsBooks;
 					});
 				});
 		
-	}
+	};
 	
-	//Delete book
-	$scope.delete = function(){
+	//Delete book;
+	$scope.delete = function(bookID, $http){
 		//Delete AJAX call goes here
 		//ng-value='UUId' carries UUID for AJAX call
+		console.log(bookID);
+		delBookServ.delete(bookID).then(function(data){
+					//SUCCESS : Updated Table displays after deleting book;	
+					if('success' == data.response.message)
+						getBookListService.getBooks().then(function(responsBooks){
+							$scope.books = responsBooks;
+					});
+		});
 	};
 });
-
-	
-	
-		
-			
--- js/factory.js
+++ js/factory.js
@@ -4,7 +4,7 @@ var mainApp = angular.module('CCapp', []);
 
 //Service for Get Book List
 mainApp.factory('getBookListService', function($http) {
-   return {
+   return ({
         getBooks: function() {
 			 var getBooksListAjax = $http.get('http://127.0.0.1:8081/api/books');
              var getBooksListResult = getBooksListAjax.then(function(result) {
@@ -18,13 +18,13 @@ mainApp.factory('getBookListService', function($http) {
              //return the promise directly.
              return getBooksListResult;
         }
-   }
+   });
 });
 
 
 //Service for Adding Book 
 mainApp.factory('addBookServ', function($http) {
-   return {
+   return ({
         add: function(bookData) {
 			 var addBookAjax = $http.post('http://127.0.0.1:8081/api/books',bookData)
 							.then(function(result) {
@@ -37,7 +37,25 @@ mainApp.factory('addBookServ', function($http) {
              //return the promise directly.
              return addBookAjax;
         }
-   }
+   });
 });
 
+
+//Service for Deleting Book 
+mainApp.factory('delBookServ', function($http) {
+   return ({
+        delete: function(bookID) {
+			 var addBookAjax = $http.delete('http://127.0.0.1:8081/api/books/'+bookID)
+							.then(function(result) {
+								//resolve the promise as the data
+								//result = JSON.parse(result);
+								if('NULL' != result){
+									return result.data;
+								}
+             });
+             //return the promise directly.
+             return addBookAjax;
+        }
+   });
+});
 
